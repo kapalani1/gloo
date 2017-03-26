e984a92954afe8e0fe6d965ac5810da4f9726af7
Karthic Palaniappan <kapalani@andrew.cmu.edu>
2017-03-26 02:33:19 -0400

    2 triangles need 2 different VAOs because cannot set same attribute(position) twice within the same vao object

--- src/gloo.cpp
+++ src/gloo.cpp
@@ -17,12 +17,20 @@ const char *vertex_shader_source =
 "   gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
 "}\n";
 
-const char *fragment_shader_source =
+const char *fragment_orange_shader_source =
 "#version 330 core\n"
 "out vec4 color;\n"
 "void main()\n"
 "{\n"
 "   color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n";
+
+const char *fragment_yellow_shader_source =
+"#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"   color = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
 "}\n";
 
 void compile_shader(GLuint shader)
@@ -40,7 +48,8 @@ void compile_shader(GLuint shader)
     }
 }
 
-GLuint setup_shaders()
+GLuint setup_shaders(const char *vertex_shader_source,
+                     const char *fragment_shader_source)
 {
     //compile shader
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -106,88 +115,107 @@ int main(int argc, char **argv)
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
     //vertices for triangle to be rendered
-    GLfloat vertices[] = {
+    GLfloat vertices[][9] = {
+        {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
-        0.5f, 0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f
+        0.5f, 0.5f, 0.0f
+        },
+        {
+        0.5f, -0.5f, 0.0f,
+        1.0f, -0.5f, 0.0f,
+        0.75f, 0.5f, 0.0f
+        }
     };
 
-    GLuint indices[] = {
-        0, 1, 2,
-        0, 2, 3
+    GLuint indices[][3] = {
+        {0, 1, 2},
+        {0, 1, 2}
     };
 
     //generate a openGl object for allocating memory on the GPU etc.
-    GLuint VBO, VAO; //store references to objects
+    GLuint VBO[2], VAO[2]; //store references to objects
 
     //don't want to repeat buffer setup code, so store in vertex array object
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    glGenVertexArrays(2, VAO);
+    glGenBuffers(2, VBO);
 
     //element buffers allow you to specify a list of vertices and indices to
     //create polygons to avoid repeating the same vertex
-    GLuint EBO;
-    glGenBuffers(1, &EBO);
+    GLuint EBO[2];
+    glGenBuffers(2, EBO);
 
-    //bind vertex array object
-    glBindVertexArray(VAO);
+    for(int i = 0; i < 2; i++)
+    {
+        //bind vertex array object
+        glBindVertexArray(VAO[i]);
 
-    //set up attributes for this VAO and which VBO to use for each VAO
-    //VBO is GPU memory that contains data used to draw
+        //set up attributes for this VAO and which VBO to use for each VAO
+        //VBO is GPU memory that contains data used to draw
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    //populate buffer (does not take VBO) because only one buffer type at a
-    //time
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO[i]);
+        //populate buffer (does not take VBO) because only one buffer type at a
+        //time
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[i]), vertices[i],
+                     GL_STATIC_DRAW);
 
-    //construct element buffer consisting of indices
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-                 GL_STATIC_DRAW);
+        //construct element buffer consisting of indices
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[i]);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[i]), indices[i],
+                     GL_STATIC_DRAW);
 
-    //tell opengl how to interpret the vertex data
-    glVertexAttribPointer(0, //location of attribute
-                          3, //size of attribute
-                          GL_FLOAT, //type of attribute
-                          GL_FALSE, //normalize data?
-                          3 * sizeof(GLfloat), //stride (space b/w consecutive
-                                               //elements of attributes array
-                          (GLvoid*)0); //offset of where attribute appears in
-                                       //data
-    //enable vertex attribute with position 0
-    glEnableVertexAttribArray(0);
+        //tell opengl how to interpret the vertex data
+        glVertexAttribPointer(0, //location of attribute
+                              3, //size of attribute
+                              GL_FLOAT, //type of attribute
+                              GL_FALSE, //normalize data?
+                              3 * sizeof(GLfloat), //stride (space b/w
+                                //consecutive elements of attributes array
+                              (GLvoid*)0); //offset of where attribute appears
+                              //in data
+        //enable vertex attribute with position 0
+        glEnableVertexAttribArray(0);
 
-    //more attribute/vbo configs here. The first parameter of the vertex
-    //attribute pointer points to the attribute and which vbo to use is given
-    //by which buffer is currently bound
+        //more attribute/vbo configs here. The first parameter of the vertex
+        //attribute pointer points to the attribute and which vbo to use is
+        //given by which buffer is currently bound
 
-    //unbind vao
-    glBindVertexArray(0);
+        //unbind vao
+        glBindVertexArray(0);
+    }
 
-    GLuint shaderProgram = setup_shaders();
+    GLuint shaderProgram[2];
+    shaderProgram[0] = setup_shaders(vertex_shader_source,
+                                     fragment_orange_shader_source);
+
+    shaderProgram[1] = setup_shaders(vertex_shader_source,
+                                     fragment_yellow_shader_source);
 
     //Wireframe mode: Replace GL_LINE with GL_FILL for default mode
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     //game loop that runs forever
     while(!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
         //check for keyboard/mouse events
         glfwPollEvents();
 
         //render here
+        glClear(GL_COLOR_BUFFER_BIT);
+        for(int i = 0; i < 2; i++)
+        {
+            glUseProgram(shaderProgram[i]);
+            glBindVertexArray(VAO[i]);
+            //glDrawArrays(GL_TRIANGLES, 0, 6);
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        }
+        glBindVertexArray(0);
 
         //double buffering ftw
         glfwSwapBuffers(window);
     }
+    glDeleteVertexArrays(2, VAO);
+    glDeleteBuffers(2, VBO);
     glfwTerminate();
     return 0;
 }
